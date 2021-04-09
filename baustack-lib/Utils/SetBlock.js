32fb7ba2ba5dbf573eef7ba9d64b99e4ce44ea72
Devandmus <contact.a.maldonado@gmail.com>
2021-04-09 15:49:07 -0400

    Update Lib

---
+++ baustack-lib/Utils/SetBlock.js
@@ -0,0 +1,41 @@
+import styles from "@lib/Library.module.scss";
+
+const SetTitle = (children) => (
+  <h2 className={styles.Set__title}>{children}</h2>
+);
+
+const SetDescription = (children) => (
+  <div className={styles.Set__docs}><p>{children}</p></div>
+);
+
+const SetFeatureList = (list) => (
+  <div className={styles.Set__properties}>
+    <h4>Propiedades</h4>
+    {!list ? (
+      "No tiene propiedades documentadas"
+    ) : (
+      <ol>
+        {list.map((el) =>
+          typeof el === "array" ? (
+            <ul>
+              {el.map((e) => (
+                <li>{e}</li>
+              ))}
+            </ul>
+          ) : (
+            <li>{el}</li>
+          )
+        )}
+      </ol>
+    )}
+  </div>
+);
+
+const SetExemple = (children) => (
+  <div className={styles.Set__elements}>{children}</div>
+);
+
+const SetBlock = (children) => <div className={styles.Set}>{children}</div>;
+
+export default SetBlock;
+export { SetTitle, SetDescription, SetFeatureList, SetExemple };
