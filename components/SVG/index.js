b61c6fac88d6485786da7aa7d56a96d77a9bfa44
Devandmus <contact.a.maldonado@gmail.com>
2021-04-09 11:54:53 -0400

    Logo baustack + redefino BoxDescription

--- components/SVG/index.js
+++ components/SVG/index.js
@@ -1,2 +1,5 @@
-export {default as AdminIcon} from './AdminIcon';
-export {default as People} from './People';
+import AdminIcon from "./AdminIcon";
+import People from "./People";
+import Logo from "./People";
+
+export { AdminIcon, People, Logo };
