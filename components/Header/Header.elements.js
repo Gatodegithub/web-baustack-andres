33579da1f70819509177cddddba13f535da9611d
leandro <aldoleandrosaavedramorales@gmail.com>
2021-05-10 15:53:45 -0400

    debug

--- components/Header/Header.elements.js
+++ components/Header/Header.elements.js
@@ -1,12 +1,12 @@
 import styled from 'styled-components';
-// import headerBg from '../SVG/HeaderBackground.svg';
-import HeaderBackground from '../SVG/HeaderBackground';
+// import headerBg from '../../public/HeaderBackground.svg';
+// import HeaderBackground from '../SVG/HeaderBackground';
 
 export const HeaderContainer = styled.header`
   height: 100vh;
   width: 100%;
   /* background: linear-gradient(106.35deg, #4316DB 0.01%, #3A0CA3 0.02%, #7209B7 99.89%); */
-  background: url(${HeaderBackground});
+  background: url('../../public/HeaderBackground.svg');
 `;
 
 export const HeaderInfoContainer = styled.div`
