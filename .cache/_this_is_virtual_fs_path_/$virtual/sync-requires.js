fe4bcec5369e76b10d8eefb6ef7a8a73d603171b
andres.maldonado <andres.maldonado@wom.cl>
2021-05-13 09:30:31 -0400

    new gatsby project

--- .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -5,6 +5,9 @@ const preferDefault = m => (m && m.default) || m
 
 exports.components = {
   "component---cache-dev-404-page-js": preferDefault(require("/Users/andresmaldonado/Documents/Baustack/web-baustack/.cache/dev-404-page.js")),
-  "component---src-pages-index-js": preferDefault(require("/Users/andresmaldonado/Documents/Baustack/web-baustack/src/pages/index.js"))
+  "component---src-pages-404-js": preferDefault(require("/Users/andresmaldonado/Documents/Baustack/web-baustack/src/pages/404.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/andresmaldonado/Documents/Baustack/web-baustack/src/pages/index.js")),
+  "component---src-pages-page-2-js": preferDefault(require("/Users/andresmaldonado/Documents/Baustack/web-baustack/src/pages/page-2.js")),
+  "component---src-pages-using-typescript-tsx": preferDefault(require("/Users/andresmaldonado/Documents/Baustack/web-baustack/src/pages/using-typescript.tsx"))
 }
 
